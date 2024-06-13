3a45b60935bc0a59dcacab2c86b89f833d98c39a
TJ Smith <tj.smith@salesloft.com>
2024-06-12 23:32:56 -0700

    Update neodim.lua

--- lua/plugins/neodim.lua
+++ lua/plugins/neodim.lua
@@ -4,11 +4,11 @@ return {
     event = "LspAttach",
     config = function()
       require("neodim").setup({
-        refresh_delay = 50, -- time in ms to wait after typing before refresh diagnostics
         alpha = 0.75,
         blend_color = "#000000",
-        hide = { underline = true, virtual_text = true, signs = true },
         disable = {}, -- table of filetypes to disable neodim
+        hide = { underline = true, virtual_text = true, signs = true },
+        refresh_delay = 50, -- time in ms to wait after typing before refresh diagnostics
       })
     end,
   },
